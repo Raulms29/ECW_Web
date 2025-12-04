469b891e43f9e32eaaccfa55976d877f9ce03d09
Raulms29 <UO287827@uniovi.es>
2025-12-04 16:40:39 +0100

    Upgrade to content and styles

--- js/time.js
+++ js/time.js
@@ -1,33 +1,44 @@
 
-function isFirefox() {
-    return navigator.userAgent.toLowerCase().includes('firefox');
+class Clock {
+    constructor() {
+        this.init();
+    }
+
+    isFirefox() {
+        return navigator.userAgent.toLowerCase().includes('firefox');
+    }
+
+    updateClock() {
+        let timeString;
+
+        if (this.isFirefox() && typeof Temporal !== 'undefined' && Temporal.Now) {
+            timeString = Temporal.Now.plainTimeISO().toString({ smallestUnit: 'second' });
+        } else {
+            timeString = new Date().toLocaleTimeString(navigator.language, { hour12: false, timeStyle: "medium" });
+        }
+
+        const footer = document.querySelector('footer');
+        let paragraphs = footer.querySelectorAll('p');
+        let clockElem;
+
+        if (paragraphs.length < 2) {
+            clockElem = document.createElement('p');
+            footer.appendChild(clockElem);
+        } else {
+            clockElem = paragraphs[1];
+        }
+
+        if (clockElem.textContent !== timeString) {
+            clockElem.textContent = timeString;
+        }
+    }
+
+    init() {
+        window.addEventListener('DOMContentLoaded', () => {
+            this.updateClock();
+            setInterval(() => this.updateClock(), 100);
+        });
+    }
 }
 
-function updateClock() {
-    let timeString;
-
-    if (isFirefox() && typeof Temporal !== 'undefined' && Temporal.Now) {
-        timeString = Temporal.Now.plainTimeISO().toString({ smallestUnit: 'second' });
-    } else {
-        timeString = new Date().toLocaleTimeString(navigator.language, { hour12: false, timeStyle: "medium" });
-    }
-
-    const $footer = $('footer');
-    let $paragraphs = $footer.find('p');
-    let $clockElem;
-
-    if ($paragraphs.length < 2) {
-        $clockElem = $('<p></p>');
-        $footer.append($clockElem);
-    } else {
-        $clockElem = $paragraphs.eq(1);
-    }
-
-
-    if ($clockElem.text() !== timeString) {
-        $clockElem.text(timeString);
-    }
-}
-
-setInterval(updateClock, 100);
-window.addEventListener('DOMContentLoaded', updateClock);
+new Clock();
